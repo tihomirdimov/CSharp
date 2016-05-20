48ce005dc9afda173f761ca77b80295916522052
tihomirdimov <tihomirdimov@tihomirdimov.com>
2016-05-20 09:06:25 +0300

    2nd commit to Problem06

--- AdvancedCSharp/AdvancedCSharpHomeworks/01StacksAndQueues/Problem06TruckTour/Problem06TruckTour.cs
+++ AdvancedCSharp/AdvancedCSharpHomeworks/01StacksAndQueues/Problem06TruckTour/Problem06TruckTour.cs
@@ -8,5 +8,21 @@ class Problem06TruckTour
     static void Main(string[] args)
     {
         int n = int.Parse(Console.ReadLine());
+        List<int[]> pumps = new List<int[]>();
+        for (int i = 0; i < n; i++)
+        {
+            int[] currentPump = Console
+                .ReadLine()
+                .Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(item => int.Parse(item))
+                .ToArray();
+            pumps.Add(currentPump);
+        }
+        foreach (int[] pump in pumps)
+        {
+            if (pump[0] > pump[1]) {
+                continue;
+            }
+        }
     }
 }
