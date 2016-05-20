1df9e95a1615e74c0c2dcf680216a20d2c4d2f4c
tihomirdimov <tihomirdimov@tihomirdimov.com>
2016-05-20 21:23:14 +0300

    Problems 03/04 Dictionaries and Sets/C# advanced updated

---
+++ AdvancedCSharp/AdvancedCSharpHomeworks/02SetsAndDictionaries/Problem03PeriodicTable/Problem03PeriodicTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Problem03PeriodicTable
+{
+    public static SortedSet<string> uniqueElements = new SortedSet<string>();
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] elements = Console
+            .ReadLine()
+            .Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .ToArray();
+            foreach (string element in elements)
+            {
+                containsElement(element);
+            }
+        }
+        foreach (string uniqueElement in uniqueElements)
+        {
+            Console.Write("{0} ", uniqueElement);
+        }
+    }
+    public static void containsElement(string currentElement)
+    {
+        if (!uniqueElements.Contains(currentElement))
+        {
+            uniqueElements.Add(currentElement);
+        }
+    }
+}
---
+++ AdvancedCSharp/AdvancedCSharpHomeworks/02SetsAndDictionaries/Problem04CountSymbols/Problem04CountSymbols.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Problem04CountSymbols
+{
+    public static SortedDictionary<char, int> occurances = new SortedDictionary<char, int>();
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        foreach (char letter in input)
+        {
+            countOccurances(letter);
+        }
+        foreach (KeyValuePair<char, int> occurance in occurances)
+        {
+            Console.WriteLine("{0}: {1} time/s", occurance.Key, occurance.Value);
+        }
+    }
+    public static void countOccurances(char letter)
+    {
+        if (occurances.ContainsKey(letter))
+        {
+            int occ = 0;
+            if (occurances.TryGetValue(letter, out occ))
+            {
+                occurances[letter] = occ + 1;
+            }
+            else
+            {
+                occurances.Add(letter, 1);
+            }
+        }
+        else
+        {
+            occurances.Add(letter, 1);
+        }
+    }
+}
