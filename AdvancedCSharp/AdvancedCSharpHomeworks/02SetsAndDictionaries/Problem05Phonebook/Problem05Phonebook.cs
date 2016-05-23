1dc7845f91a049b7b172a7dafa9b6b767b255375
tihomirdimov <tihomirdimov@tihomirdimov.com>
2016-05-23 15:22:51 +0300

    Problem 5,6,7 from Dictionaries and Sets

---
+++ AdvancedCSharp/AdvancedCSharpHomeworks/02SetsAndDictionaries/Problem05Phonebook/Problem05Phonebook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Problem05Phonebook
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        Dictionary<String, String> phonebook = new Dictionary<string, string>();
+        while (input != "search")
+        {
+            string[] elements = input
+            .Split(new Char[] { '-' }, StringSplitOptions.RemoveEmptyEntries)
+            .ToArray();
+            phonebook.Add(elements[0], elements[1]);
+            input = Console.ReadLine();
+        }
+        input = Console.ReadLine();
+        string phoneNumber;
+        while (input != "stop")
+        {
+            if (phonebook.TryGetValue(input, out phoneNumber))
+            {
+                Console.WriteLine("{0} -> {1}", input, phoneNumber);
+            }
+            else
+            {
+                Console.WriteLine("Contact {0} does not exist.", input);
+            }
+            input = Console.ReadLine();
+        }
+    }
+}
---
+++ AdvancedCSharp/AdvancedCSharpHomeworks/02SetsAndDictionaries/Problem06AMinerTask/Problem06AMinerTask.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem06AMinerTask
+{
+    class Problem06AMinerTask
+    {
+        public static Dictionary<string, int> resources = new Dictionary<string, int>();
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string tempResource = "";
+            int tempQuantity = 0;
+            int counter = 1;
+            while (input != "stop")
+            {
+                if (counter % 2 != 0)
+                {
+                    tempResource = input;
+                    counter++;
+                }
+                else
+                {
+                    tempQuantity = int.Parse(input);
+                    isInDictionary(tempResource, tempQuantity);
+                    counter++;
+                }
+                input = Console.ReadLine();
+            }
+            foreach (KeyValuePair<string, int> resource in resources)
+            {
+                Console.WriteLine("{0} -> {1}", resource.Key, resource.Value );
+            }
+        }
+        public static void isInDictionary(string tempStr, int tempInt)
+        {
+            if (resources.ContainsKey(tempStr))
+            {
+                resources[tempStr] += tempInt;
+            }
+            else
+            {
+                resources.Add(tempStr, tempInt);
+            }
+        }
+    }
+}
