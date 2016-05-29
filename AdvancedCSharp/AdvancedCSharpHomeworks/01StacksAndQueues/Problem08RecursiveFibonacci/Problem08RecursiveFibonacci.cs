6c7e72751441899a669f1e9e0c635e7777c6442c
tihomirdimov <tihomirdimov@tihomirdimov.com>
2016-05-29 18:32:33 +0300

    Recursive Fibonacci

---
+++ AdvancedCSharp/AdvancedCSharpHomeworks/01StacksAndQueues/Problem08RecursiveFibonacci/Problem08RecursiveFibonacci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Problem08RecursiveFibonacci
+{
+    public static Stack<long> fibNumbers = new Stack<long>();
+    static void Main()
+    {
+        long input = long.Parse(Console.ReadLine());
+        Console.WriteLine(calculateFibonacci(input));
+    }
+    public static long calculateFibonacci(long n)
+    {
+        if (n <= 2)
+        {
+            return 1;
+        }
+        else
+        {
+            n = calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
+            return n;
+        }
+    }
+}
