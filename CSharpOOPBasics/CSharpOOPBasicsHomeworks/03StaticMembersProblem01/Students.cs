959e326184bd953326c92730f62ed110c8fc01ad
tihomirdimov <tihomirdimov@tihomirdimov.com>
2016-07-04 08:51:31 +0300

    Static members problem 01

---
+++ CSharpOOPBasics/CSharpOOPBasicsHomeworks/03StaticMembersProblem01/Students.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03StaticMembersProblem01
+{
+    class Student
+    {
+        static int counter = 0;
+        string name;
+        public static int getCounter
+        {
+            get { return Student.counter; }
+        }
+        public Student(string name)
+        {
+            Student.counter++;
+            this.name = name;
+        }
+    }
+    class Students
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            while (input != "End")
+            {
+                new Student(input);
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(Student.getCounter);
+        }
+    }
+}
