dffae44f192fae755f4caf57908bd15dd8673719
tihomirdimov <tihomirdimov@tihomirdimov.com>
2016-07-07 23:18:49 +0300

    structure for problme 03 Wild Farm from Polymorphism added

---
+++ CSharpOOPBasics/CSharpOOPBasicsHomeworks/06PolymorphismProblem03/WildFarm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06PolymorphismProblem03
+{
+    class WildFarm
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
