55148aae4b0d93efac119db592a62ae4e2298d6d
tihomirdimov <tihomirdimov@tihomirdimov.com>
2016-07-17 11:35:26 +0300

    interfaces problem02

--- CSharpOOPAdvanced/CSharpOOPAdvancedHomeworks/01InterfacesAndAbstractionProblme02/Startup.cs
+++ CSharpOOPAdvanced/CSharpOOPAdvancedHomeworks/01InterfacesAndAbstractionProblme02/Startup.cs
@@ -1,15 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _01InterfacesAndAbstractionProblme02
 {
+    interface IPerson
+    {
+        string Name { get; }
+        int Age { get; }
+    }
+
+    interface IIdentifiable
+    {
+        string Id { get; }
+    }
+
+    interface IBirthable
+    {
+        string Birthdate { get; }
+    }
+
+    class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthdate;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Id { get; set; }
+        public string Birthdate { get; set; }
+    }
     class Startup
     {
         static void Main()
         {
+            Type identifiableInterface = typeof(Citizen).GetInterface("IIdentifiable");
+            Type birthableInterface = typeof(Citizen).GetInterface("IBirthable");
+            PropertyInfo[] properties = identifiableInterface.GetProperties();
+            Console.WriteLine(properties.Length);
+            Console.WriteLine(properties[0].PropertyType.Name);
+            properties = birthableInterface.GetProperties();
+            Console.WriteLine(properties.Length);
+            Console.WriteLine(properties[0].PropertyType.Name);
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string id = Console.ReadLine();
+            string birthdate = Console.ReadLine();
+            IIdentifiable identifiable = new Citizen(name, age, id, birthdate);
+            IBirthable birthable = new Citizen(name, age, id, birthdate);
         }
     }
 }
---
+++ CSharpOOPAdvanced/CSharpOOPAdvancedHomeworks/02GenericsProblem08/Srartup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02GenericsProblem08
+{
+    class Srartup
+    {
+        public class CustomList<T>
+            where T : IComparable<T>
+        {
+        }
+        static void Main()
+        {
+        }
+    }
+}
