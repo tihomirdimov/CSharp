19a4682845fc3ddea992161f7e92ed0480bdfd1c
tihomirdimov <tihomirdimov@tihomirdimov.com>
2016-07-18 23:50:17 +0300

    problem 01 iterators body created

---
+++ CSharpOOPAdvanced/CSharpOOPAdvancedHomeworks/03IteratorsAndComparatorsProblem01/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03IteratorsAndComparatorsProblem01
+{
+    public class ListyIterator
+    {
+        Collection<T> collection;
+        public ListyIterator(Collection<T> collection)
+        {
+            
+        }
+    }
+    class Startup
+
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
