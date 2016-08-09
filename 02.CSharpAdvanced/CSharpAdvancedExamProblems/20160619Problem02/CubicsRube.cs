8526cad13fb7f9a348f8cdaa11c64d5944a1bf15
tihomirdimov <tihomirdimov@tihomirdimov.com>
2016-08-10 00:07:09 +0300

    19.06.2016 exam problems 02 and 03

---
+++ 02.CSharpAdvanced/CSharpAdvancedExamProblems/20160619Problem02/CubicsRube.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _20160619Problem02
+{
+    class CubicsRube
+    {
+        static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+            int cellCount = size * size * size;
+            long cellsWithParticleSum = 0;
+            string input = Console.ReadLine();
+            while (input != "Analyze")
+            {
+                string[] particles = input.Split(' ');
+                int x = int.Parse(particles[0]);
+                int y = int.Parse(particles[1]);
+                int z = int.Parse(particles[2]);
+                long particleValue = long.Parse(particles[3]);
+                if (checkIsValidCell(size, x, y, z, particleValue))
+                {
+                    cellCount--;
+                    cellsWithParticleSum += particleValue;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(cellsWithParticleSum);
+            Console.WriteLine(cellCount);
+        }
+        public static bool checkIsValidCell(int size, int x, int y, int z, long value )
+        {
+            if (x < size && x >= 0 && y < size && y >= 0 && z < size && z >= 0 && value !=0 )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ 02.CSharpAdvanced/CSharpAdvancedExamProblems/20160619Problem03/CubicMessages.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _20160619Problem03
+{
+    class CubicMessages
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            while (!input.Equals("Over!"))
+            {
+                int charNum = int.Parse(Console.ReadLine());
+                string pattern = string.Format("^([0-9]+)([a-zA-Z]{{{0}}})([^a-zA-Z]*)$", charNum);
+                if (Regex.IsMatch(input, pattern))
+                {
+                    Regex regex = new Regex(pattern);
+                    Match match = regex.Match(input);
+                    char[] numbersBefore = match.Groups[1].Value.ToCharArray();
+                    var msg = match.Groups[2].Value;
+                    char[] numbersAfter = match.Groups[3].Value.ToCharArray();
+                    StringBuilder result = new StringBuilder();
+                    foreach (var item in numbersBefore)
+                    {
+                        int num = int.Parse(item.ToString());
+                        if (num >= 0 && num < msg.Length)
+                        {
+                            result.Append(msg[num]);
+                        }
+                        else
+                        {
+                            result.Append(' ');
+                        }
+                    }
+                    foreach (var item in numbersAfter)
+                    {
+                        if (Char.IsDigit(item))
+                        {
+                            int num = int.Parse(item.ToString());
+                            if (num >= 0 && num < msg.Length)
+                            {
+                                result.Append(msg[num]);
+                            }
+                            else
+                            {
+                                result.Append(' ');
+                            }
+                        }
+                    }
+                    Console.WriteLine("{0} == {1}", msg, result);
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
