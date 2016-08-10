39175f2f36df919f3d1e09d1a270dbbb7d33b8de
tihomirdimov <tihomirdimov@tihomirdimov.com>
2016-08-10 09:36:39 +0300

    problem 04 cubic assault added

---
+++ 02.CSharpAdvanced/CSharpAdvancedExamProblems/20160619Problem04/CubicAssault.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _20160619Problem04
+{
+    class CubicAssault
+    {
+        public static Dictionary<string, int[]> regionsDb = new Dictionary<string, int[]>();
+        static void Main(string[] args)
+        {
+            string patternGreen = "(.*)\\s*\\-\\>\\s*(Green)\\s*\\-\\>\\s*([0-9]+)";
+            string patternRed = "(.*)\\s*\\-\\>\\s*(Red)\\s*\\-\\>\\s*([0-9]+)";
+            string patternBlack = "(.*)\\s*\\-\\>\\s*(Black)\\s*\\-\\>\\s*([0-9]+)";
+            
+            string input = Console.ReadLine();
+            while (!input.Equals("Count em all"))
+            {
+                if (Regex.IsMatch(input, patternGreen))
+                {
+                    Regex regex = new Regex(patternGreen);
+                    Match match = regex.Match(input);
+                    AddRegion(match.Groups[1].Value);
+                }
+                else if (Regex.IsMatch(input, patternRed))
+                {
+                    Regex regex = new Regex(patternRed);
+                    Match match = regex.Match(input);
+                    AddRegion(match.Groups[1].Value);
+                }
+                else if (Regex.IsMatch(input, patternBlack))
+                {
+                    Regex regex = new Regex(patternBlack);
+                    Match match = regex.Match(input);
+                    AddRegion(match.Groups[1].Value);
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var region in regionsDb)
+            {
+                Console.WriteLine(region.Key);
+            }
+        }
+        public static void AddRegion(string region)
+        {
+            //Array indexes 0-green, 1-red, 2-black
+            if (!regionsDb.ContainsKey(region))
+            {
+                regionsDb.Add(region, new int[3]);
+            }
+            //else
+            //{
+            //    if (type.Equals("red", StringComparison.CurrentCultureIgnoreCase))
+            //    {
+            //        var temp = regionsDb[region].GetValue();
+            //    }
+            //}
+        }
+    }
+}
