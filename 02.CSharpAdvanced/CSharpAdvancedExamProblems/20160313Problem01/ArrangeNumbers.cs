332949fad47f49fd03a52397df7b5b73cc18afbc
tihomirdimov <tihomirdimov@tihomirdimov.com>
2016-08-11 23:07:04 +0300

    add problems from test exam - 13 06 2016

---
+++ 02.CSharpAdvanced/CSharpAdvancedExamProblems/20160313Problem01/ArrangeNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20160313Problem01
+{
+    class ArrangeNumbers
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
