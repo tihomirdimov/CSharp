35c529a3106cd32175bfd3889d5f8b18a9e8fced
tihomirdimov <tihomirdimov@tihomirdimov.com>
2016-09-01 00:03:32 +0300

    validation for adding software components

---
+++ 03.CSharpOOPBasics/CSharpOOPBasicsExamProblems/20160710/20160710/Commands/SoftwareComponentValidation.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using _20160710.Models;
+
+namespace _20160710.Commands
+{
+    public class SoftwareComponentValidation
+    {
+        private Software software;
+        private string hardware;
+        public SoftwareComponentValidation(Software software, string hardware)
+        {
+            this.software = software;
+            this.hardware = hardware;
+        }
+        public bool IsValid()
+        {
+            if (TheSystem.Components.Any(s => s.Name.Equals(hardware)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
