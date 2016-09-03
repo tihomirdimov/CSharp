7ec6c5173f7877e8d31694629995911033c6d91a
tihomirdimov <tihomirdimov@tihomirdimov.com>
2016-09-03 09:08:57 +0300

    small change

--- 03.CSharpOOPBasics/CSharpOOPBasicsExamProblems/20160710/20160710/Commands/RegisterExpressSoftware.cs
+++ 03.CSharpOOPBasics/CSharpOOPBasicsExamProblems/20160710/20160710/Commands/RegisterExpressSoftware.cs
@@ -13,7 +13,7 @@
             SoftwareComponentValidation validate = new SoftwareComponentValidation(software, software.Hardware);
             if (validate.IsValid())
             {
-                TheSystem.Components.Find(c => c.Name == software.Hardware).Software.Add(software);
+                TheSystem.Components.Find(c => c.Name.Equals(software.Hardware)).Software.Add(software);
             }
         }
     }
--- 03.CSharpOOPBasics/CSharpOOPBasicsExamProblems/20160825/20160825/Models/Centers/CleansingCenter.cs
+++ 03.CSharpOOPBasics/CSharpOOPBasicsExamProblems/20160825/20160825/Models/Centers/CleansingCenter.cs
@@ -1,7 +1,5 @@
 ﻿namespace _20160825.Models.Centers
 {
-    using System.Collections.Generic;
-    using Animals;
     public class CleansingCenter : Center
     {
         public CleansingCenter(string name) : base(name)
