4a7ab2016050a1ce4f578d04a10b0a9f267818a5
tihomirdimov <tihomirdimov@tihomirdimov.com>
2017-02-12 13:15:12 +0200

    back-end part 1

--- 05.CSharpWeb/01.Homeworks/04.WebAndEntityFramework/PizzaMore/PizzaMore.Data/Models/Session.cs
+++ 05.CSharpWeb/01.Homeworks/04.WebAndEntityFramework/PizzaMore/PizzaMore.Data/Models/Session.cs
@@ -1,6 +1,18 @@
-﻿namespace PizzaMore.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaMore.Data.Models
 {
-    class Session
+    public class Session
     {
+        [Key]
+        public string Id { get; set; }
+
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Id}\t{this.User.Id}";
+        }
     }
 }
--- 05.CSharpWeb/01.Homeworks/04.WebAndEntityFramework/PizzaMore/PizzaMore.Data/PizzaMoreContext.cs
+++ 05.CSharpWeb/01.Homeworks/04.WebAndEntityFramework/PizzaMore/PizzaMore.Data/PizzaMoreContext.cs
@@ -3,29 +3,17 @@ namespace PizzaMore.Data
     using System;
     using System.Data.Entity;
     using System.Linq;
+    using PizzaMore.Data.Models;
 
     public class PizzaMoreContext : DbContext
-    {
-        // Your context has been configured to use a 'PizzaMoreContext' connection string from your application's 
-        // configuration file (App.config or Web.config). By default, this connection string targets the 
-        // 'PizzaMore.Data.PizzaMoreContext' database on your LocalDb instance. 
-        // 
-        // If you wish to target a different database and/or database provider, modify the 'PizzaMoreContext' 
-        // connection string in the application configuration file.
+    {   
         public PizzaMoreContext()
             : base("name=PizzaMoreContext")
         {
         }
 
-        // Add a DbSet for each entity type that you want to include in your model. For more information 
-        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
-
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Session> Sessions { get; set; }
+        public virtual DbSet<Pizza> PizzaSuggestions { get; set; }
     }
-
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
 }
