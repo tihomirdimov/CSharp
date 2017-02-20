1bcb357493a99a28219c47e84e6d4eb48a8e18dd
tihomirdimov <tihomirdimov@tihomirdimov.com>
2017-02-20 22:15:12 +0200

    MVC workshop skeleton added

---
+++ 05.CSharpWeb/01.Homeworks/06.IntroductionToMvc/MVC-SharpStore-Skeleton/SimpleMVC.App/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using SimpleMVC.App.MVC.Attributes.Methods;
+using SimpleMVC.App.MVC.Controllers;
+using SimpleMVC.App.MVC.Interfaces;
+
+namespace SimpleMVC.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 05.CSharpWeb/01.Homeworks/06.IntroductionToMvc/MVC-SharpStore-Skeleton/SimpleMVC.App/Views/About/About.cs
@@ -0,0 +1,13 @@
+﻿using SimpleMVC.App.MVC.Interfaces;
+using System.IO;
+
+namespace SimpleMVC.App.Views.About
+{
+    public class About : IRenderable
+    {
+        public string Render()
+        {
+            return File.ReadAllText("../../content/about.html");
+        }
+    }
+}
